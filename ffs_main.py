fa6b27cabc1cb04a1143c9ff0c00724ac477d7fe
thewhitetulip <sapatil@live.com>
2015-05-18 07:10:48 +0530

    error handling & win/mac support

--- ffs_main.py
+++ ffs_main.py
@@ -5,6 +5,7 @@ import sqlite3
 import os
 #import json
 from datetime import datetime
+import sys
 
 def history(cursor, today=False):
     sql="""select url, title, last_visit_date  from moz_historyvisits natural join moz_places where url  like '%http%' and last_visit_date is not null
@@ -49,12 +50,21 @@ where moz_places.url  like 'http%' and visit_count>0 order by last_visit_date de
 
 if __name__=="__main__":
     home_dir = os.environ['HOME']
-    firefox_path = home_dir + "/.mozilla/firefox/"
+    if sys.platform.startswith('win') == True:
+        firefox_path = home_dir + '\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\'
+    elif sys.platform.startswith('linux')==True:
+        firefox_path = home_dir + "/.mozilla/firefox/"
+    elif sys.platform.startswith('darwin')==True:
+        firefox_path = home_dir+'Library/Application Support/Firefox/Profiles/'
+    
     profiles = [i for i in os.listdir(firefox_path) if i.endswith('.default')]
     sqlite_path = firefox_path+ profiles[0]+'/places.sqlite'
-    print(sqlite_path)
-
-    connection = sqlite3.connect(sqlite_path)
+     
+    try:
+        connection = sqlite3.connect(sqlite_path)
+    except:
+        print('Something went wrong with places.sqlite')
+        exit(1)
 
     cursor = connection.cursor()
     history(cursor, today=True)
